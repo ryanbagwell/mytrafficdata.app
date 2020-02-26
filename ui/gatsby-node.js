e7382289f8a9fccf8a0722f7eccdb2090ba393e7
Ryan Bagwell <ryan@ryanbagwell.com>
2020-02-26 14:07:43 -0500

    fix registry

--- ui/gatsby-node.js
+++ ui/gatsby-node.js
@@ -1,7 +1,18 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
-// You can delete this file if you're not using it
+exports.onCreateWebpackConfig = ({
+  stage,
+  actions,
+  getConfig
+}) => {
+  if (stage === 'build-html') {
+    actions.setWebpackConfig({
+      externals: getConfig().externals.concat(function(context, request, callback) {
+        const regex = /^@?firebase(\/(.+))?/;
+        // exclude firebase products from being bundled, so they will be loaded using require() at runtime.
+        if (regex.test(request)) {
+          return callback(null, 'umd ' + request);
+        }
+        callback();
+      })
+    });
+  }
+};
