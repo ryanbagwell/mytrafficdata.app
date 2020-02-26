d79ae15294ce2000f57d783e7fe87e9db10fe77b
Ryan Bagwell <ryan@ryanbagwell.com>
2020-02-26 18:44:01 -0500

    latest

--- daemon/measurementQueue/index.js
+++ daemon/measurementQueue/index.js
@@ -56,8 +56,6 @@ class InboundMeasurementQueue extends BaseMeasurementQueue {
     let {queue} = this;
     let initialReport = queue[0];
 
-    console.log(this.queue);
-
     // only one item in the queue means
     // the vehicle has just started to enter the
     // field of vision, so just update the live
