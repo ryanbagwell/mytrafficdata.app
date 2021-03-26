6f90bf319dae46d9a004163d2883e32deae45485
Ryan Bagwell <ryan@ryanbagwell.com>
2021-03-26 09:26:02 -0400

    use import statements

--- ui/src/utils/getDB.js
+++ ui/src/utils/getDB.js
@@ -1,11 +1,5 @@
-const firebase = require("firebase")
-require("firebase/database")
+import getFirebaseApp from "./getFirebaseApp"
 
-var firebaseConfig = {
-  apiKey: process.env.GATSBY_FIREBASE_API_KEY,
-  databaseURL: process.env.GATSBY_FIREBASE_DATABASE_URL,
-}
+const app = getFirebaseApp()
 
-firebase.initializeApp(firebaseConfig)
-
-export default () => firebase.database()
+export default () => app.database()
