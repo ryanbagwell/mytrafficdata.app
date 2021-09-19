8fa98e721ef2b036c787a92c67051e4010e62a3e
Ryan Bagwell <ryan@ryanbagwell.com>
2021-09-19 10:28:02 -0400

    add some logging for debug

--- ui/src/utils/getFirebase.ts
+++ ui/src/utils/getFirebase.ts
@@ -2,14 +2,12 @@ let instance = null;
 
 export default async () => {
   if (instance || typeof window === 'undefined') return instance;
-  console.log('getFirebase')
   const fb = await import('firebase/dist/index.esm.js')
   await import("firebase/firestore");
   await import("firebase/database");
   await import("firebase/auth");
   instance = fb.default
-  console.log('instance', instance)
-  try {
+   try {
     instance.app()
   } catch {
     instance.initializeApp({
@@ -19,5 +17,10 @@ export default async () => {
       projectId: process.env.GATSBY_FIREBASE_PROJECT_ID,
     })
   }
+  console.log({apiKey: process.env.GATSBY_FIREBASE_API_KEY,
+    databaseURL: process.env.GATSBY_FIREBASE_DATABASE_URL,
+    authDomain: process.env.GATSBY_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.GATSBY_FIREBASE_PROJECT_ID,
+  })
   return instance
 }
