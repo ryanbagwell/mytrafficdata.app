5740275f1844cee4204bd7d36dc7d820bda8aaef
Ryan Bagwell <ryan@ryanbagwell.com>
2021-09-29 15:05:46 -0400

    update logging

--- daemon/logger/index.js
+++ daemon/logger/index.js
@@ -1,10 +1,14 @@
 const winston = require('winston');
+const {combine, timestamp, cli} = winston.format;
 
 winston.level = process.env.LOG_LEVEL || 'debug';
 
 const logger = winston.createLogger({
   levels: winston.config.syslog.levels,
-  format: winston.format.cli(),
+  format: combine(
+    timestamp(),
+    cli(),
+  ),
   transports: [
     new winston.transports.Console({ level: 'debug' }),
     new winston.transports.Console({ level: 'info' }),
